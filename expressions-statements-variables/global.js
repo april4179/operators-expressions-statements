d2272e98e9fb039ca199dcb0c7cf2682f6e62a6d
april e chilldres <55533435+april4179@users.noreply.github.com>
2020-07-09 12:44:52 -0500

    first commit

---
+++ expressions-statements-variables/global.js
@@ -0,0 +1,56 @@
+// Welcome! Here's an example of a console logged response
+console.log(4 + 5);
+
+/***********************/
+/* Using Expressions   */
+/***********************/
+
+//1.) Log an expression that evaluates to the sum of two integers
+// your code here
+let a = 1 
+console.log(1 + 0);
+
+// 2.) log an expression that evaluates to the difference of two integers
+// your code here
+let b = 2
+console.log(4 - 2);
+
+// 3.) Log an expression that concatenates the two strings "I'm writing" and "my first JavaScript code"
+// your code here
+const str1 = "I'm writing"
+const str2 = "my first JavaScript code"
+console.log("I'm writing my first JavaScript code");
+
+// 4.) Log an expression that evaluates to true or false using two numbers that are greater or less than eachother
+// your code here
+4 < 6
+console.log("true");
+
+// 5.) Log an expression that uses modulo, with a result of 4
+// your code here
+1024 % 10 
+console.log("4");
+
+// 6.) Create two different strings and compare them, logging the result.
+// It should be false!
+true && false
+!false
+console.log("false");
+
+// 7.) Concatenate 4 different strings to form the sentence "I'm learning a bit of JavaScript at Kenzie"
+concat str1 = "I'm learning"
+concat str2 = "a bit of"
+concat str3 = "JavaScript"
+concat str4 = "at Kenzie"
+console.log("I'm learning a bit of JavaScript at Kenzie");
+
+ 5 < 10
+ 8 > 5
+ console.log(true)
+// The expression should represent: 5 is less than 10 and 8 is greater than 5
+// This should print true
+
+// console.log("answer 8")
+
+// 9.) Add parenthesis to the following statement, the result should be 1
+console.log(3 * 3 + 1) /(20 / 2) == 1; // This should log true
